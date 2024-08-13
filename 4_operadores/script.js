fff6e06b3d6859a7ed7deeaad1bb3a08eef796b5
andreschaparro <chaparroandres87@gmail.com>
2024-08-13 19:40:58 -0300

    Operadores

---
+++ 4_operadores/script.js
@@ -0,0 +1,31 @@
+// OPERADORES
+
+const variable = 'Valor de la variable' // ASIGNACION
+
+const suma = 1 + 3 // OPERADOR ARITMETICO DE LA SUMA
+const resta = 4 - 1 // OPERADOR ARITMETICO DE LA RESTA
+const multiplicacion = 1 * 5 // OPERADOR ARITMETICO DE LA MULTIPLICACION
+const division = 6 / 3 // OPERADOR ARITMETICO DE LA DIVISION
+const resto = 5 % 2 // OPERADOR ARITMETICO RESTO DE UNA DIVISION
+const potencia = 3 ** 2 // OPERADOR ARITMETICO POTENCIA
+
+let x = 5;
+let y = 5;
+x++; // OPERADOR AUMENTADOR (INCREMENTO EN 1)
+y--; // OPERADOR DISMINUIDOR (DECREMENTO EN 1)
+
+// COMANDO UTIL DE VISUAL STUDIO CODE:
+// alt+shit+down arrow
+
+// EN FIREFOX
+// ctrl+shift+i para ver la Consola
+
+console.log(variable)
+console.log(suma)
+console.log(resta)
+console.log(multiplicacion)
+console.log(division)
+console.log(resto)
+console.log(potencia)
+console.log(x)
+console.log(y)
