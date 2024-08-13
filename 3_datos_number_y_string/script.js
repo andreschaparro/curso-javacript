1561978cd2981e461a190126c42877735da68e62
andreschaparro <chaparroandres87@gmail.com>
2024-08-13 19:26:24 -0300

    Datos Number y String

---
+++ 3_datos_number_y_string/script.js
@@ -0,0 +1,8 @@
+// NUMEROS
+let variableNumero = 10 // valor entero
+let variableDecimal = 10.10 // valor flotante
+
+// TEXTO
+let variableTexto = 'Texto' // valor string (cadena de caracteres)
+let variableTexto2 = "Texto" // valor string (cadena de caracteres)
+let variableTexto3 = `Esta es una variable distinta ${variableNumero}` // template literals (permite incrustar otras variables en un string)
