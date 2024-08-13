872639b1e9cfd97078ad7b82e02a8953a2ccd9c1
andreschaparro <chaparroandres87@gmail.com>
2024-08-13 19:52:48 -0300

    Sintaxis Variables

---
+++ 5_sintaxis_variables/script.js
@@ -0,0 +1,22 @@
+/*
+    COMENTARIO MULTI-LINEA
+
+    REGLAS PARA NOMBRE DE VARIABLES:
+
+    1. Se puede iniciar con $, minuscula, mayuscula, y _
+    2. No se puede iniciar con numero pero si usarlo en el resto del nombre
+    3. No se pueden usar caracteres especiales excepto $ y _
+*/
+
+const $variable = 'Nombre'
+const variable = 'Nombre'
+const Variable = 'Nombre'
+const _variable = 'Nombre'
+
+// CASE SENSITIVE
+
+let nombreVariable = 'Pedro'
+let nombrevariable
+
+console.log(nombreVariable)
+console.log(nombrevariable)
