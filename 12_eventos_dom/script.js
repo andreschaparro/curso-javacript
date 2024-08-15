d4f4dac629548c384f410cb3f0f95dc8532fa1c3
andreschaparro <chaparroandres87@gmail.com>
2024-08-15 20:40:12 -0300

    Eventos DOM

---
+++ 12_eventos_dom/script.js
@@ -0,0 +1,6 @@
+function mostrar(){
+    // buscamos el elemento con id igual a "seleccionable" y le cambia el texto por la fecha actual
+    document.getElementById('seleccionable').innerHTML = Date();
+}
+
+// Listado de eventos: https://www.w3schools.com/tags/ref_eventattributes.asp
