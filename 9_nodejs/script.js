988da60b783a0ee8445d34b86dcd1f456dce2069
Andres Chaparro <chaparroandres87@gmail.com>
2024-09-24 15:54:22 -0300

    Correcciones 5

--- 9_nodejs/script.js
+++ 9_nodejs/script.js
@@ -1,4 +1,4 @@
-// Abrir una terminal en el directorio donde esta el archivo script.js
+// Abrir una terminal en el directorio donde se encuentra el archivo script.js
 // Ejecutar: node script.js
 const texto = 'Este texto va a salir por consola'
 console.log(texto)
