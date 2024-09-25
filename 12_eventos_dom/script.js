75a3115a870bd4dfbbaa7cefcb1f5d9785fdcde0
andreschaparro <chaparroandres87@gmail.com>
2024-09-25 10:26:20 -0300

    Correcciones 8

--- 12_eventos_dom/script.js
+++ 12_eventos_dom/script.js
@@ -1,6 +1,5 @@
-function mostrar(){
-    // buscamos el elemento con id igual a "seleccionable" y le cambia el texto por la fecha actual
-    document.getElementById('seleccionable').innerHTML = Date();
-}
-
 // Listado de eventos: https://www.w3schools.com/tags/ref_eventattributes.asp
+
+function actualizarFecha() {
+    document.getElementById('fecha').innerHTML = Date();
+}
