94668f495b82a1e988b92c2aaf2eb0f97d19553a
andreschaparro <chaparroandres87@gmail.com>
2024-09-30 15:30:26 -0300

    Correcciones 23

--- 24_callbacks/script.js
+++ 24_callbacks/script.js
@@ -1,16 +1,11 @@
-// CALLBACKS
-// EJECUTAR node script.js
-
-// FUNCION DE CALLBACK
 function mostrarConsola(num) {
-    console.log(num);
+    console.log(num)
 }
 
-// FUNCION QUE RECIBE DOS NUMEROS Y UNA FUNCION QUE SE EJECUTARA LUEGO QUE ESTA FINALICE
 function calcular(num1, num2, callback) {
-    let suma = num1 + num2;
-    callback(suma);
+    let suma = num1 + num2
+    callback(suma)
 }
 
-// CUANDO PASAMOS UNA FUNCION COMO PARAMETRO DECIMOS QUE ESTAMOS TRABAJANDO CON CALLBACKS
-calcular(1, 2, mostrarConsola);
+// Cuando pasamos una función como parámetro a la misma se la llama función de callback
+calcular(1, 2, mostrarConsola)
