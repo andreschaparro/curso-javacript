6b425b38608a46b8a5ce41196df0759e127c5a50
andreschaparro <chaparroandres87@gmail.com>
2024-10-01 08:51:39 -0300

    Correcciones 23

--- 26_async_await/script.js
+++ 26_async_await/script.js
@@ -1,45 +1,42 @@
-// PROMESAS CON ASYNC-AWAIT PERMITE SOLUCIONAR EL PROBLEMA DE LA PIRAMIDE DE LA PERDICION
-// EJECUTAR node script.js
+// En las siguientes líneas se utiliza async para definir una función asíncrona y await para esperar las respuestas dentro de la misma
+// Su uso nos evita caer en la pirámide de la perdición
 let promesa1 = new Promise((response, reject) => {
     setTimeout(() => {
         let resp = {
             response: 200,
-            description: 'Esta info es importante',
-        };
-        response(resp);
-        reject('Fallo 1');
-    }, 3000);
-});
+            description: 'Salió todo bien 1',
+        }
+        response(resp)
+    }, 5000)
+})
 
 let promesa2 = new Promise((response, reject) => {
     setTimeout(() => {
         let resp = {
             response: 200,
-            description: 'Esta info es importante pero suele demorar mucho',
-        };
-        response(resp);
-        reject('Fallo 2');
-    }, 5000);
-});
+            description: 'Salió todo bien 2',
+        }
+        response(resp)
+    }, 7500)
+})
 
 let promesa3 = new Promise((response, reject) => {
     setTimeout(() => {
         let resp = {
             response: 200,
-            description: 'Información rápida',
-        };
-        response(resp);
-        reject('Fallo 3');
-    }, 2500);
-});
+            description: 'Salió todo bien 3',
+        }
+        response(resp)
+    }, 2500)
+})
 
-async function ejecutarPromesas() { // Ya no hace falta armar la estructura anidada sino que se define una función del tipo async
-    let respuestaProm1 = await promesa1; // El await indica que se debe esperar el response de la promesa antes de proseguir la ejecución
-    console.log(respuestaProm1);
-    let respuestaProm2 = await promesa2;
-    console.log(respuestaProm2);
-    let respuestaProm3 = await promesa3;
-    console.log(respuestaProm3);
+async function ejecutarPromesas() {
+    let respuestaProm1 = await promesa1
+    console.log(respuestaProm1)
+    let respuestaProm2 = await promesa2
+    console.log(respuestaProm2)
+    let respuestaProm3 = await promesa3
+    console.log(respuestaProm3)
 }
 
-ejecutarPromesas() // Llamamos a la función
+ejecutarPromesas()
